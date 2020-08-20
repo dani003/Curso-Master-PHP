5a430c4a5814fbebf402a036a66ddafd4c5ee3e5
Daniela <daniela.ramirez003@gmail.com>
2020-08-19 22:08:10 -0400

    Comenzando ORM

--- proyecto-laravel/database.sql
+++ proyecto-laravel/database.sql
@@ -16,6 +16,10 @@ remember_token  varchar(255),
 CONSTRAINT pk_users PRIMARY KEY(id)
 )ENGINE=InnoDb;
 
+INSERT INTO users VALUES(NULL, 'user','Daniela', 'Ramirez', 'DanielaWeb', 'Daniela@gmail.com', '123', NULL, CURTIME(), CURTIME(), NULL);
+INSERT INTO users VALUES(NULL, 'user','Ariel', 'Silva', 'ArielNick', 'Ariel@gmail.com', '123', NULL, CURTIME(), CURTIME(), NULL);
+INSERT INTO users VALUES(NULL, 'user','Martin', 'Ramirez', 'MartinKratos', 'Martin@gmail.com', '123', NULL, CURTIME(), CURTIME(), NULL);
+
 
 CREATE TABLE IF NOT EXISTS images(
 id              int(255) auto_increment not null,
@@ -27,6 +31,12 @@ updated_at      datetime,
 CONSTRAINT pk_images PRIMARY KEY(id),
 CONSTRAINT fk_images_users FOREIGN KEY(user_id) REFERENCES users(id)
 )ENGINE=InnoDb;
+
+INSERT INTO images VALUES(NULL, 1, 'test.jpg', 'descripcion de prueba', CURTIME(), CURTIME());
+INSERT INTO images VALUES(NULL, 1, 'playa.jpg', 'descripcion de prueba playa', CURTIME(), CURTIME());
+INSERT INTO images VALUES(NULL, 1, 'arena.jpg', 'descripcion de prueba arena', CURTIME(), CURTIME());
+INSERT INTO images VALUES(NULL, 3, 'familia.jpg', 'descripcion de prueba familia', CURTIME(), CURTIME());
+
 
 CREATE TABLE IF NOT EXISTS comments(
 id              int(255) auto_increment not null,
@@ -40,6 +50,10 @@ CONSTRAINT fk_comments_users FOREIGN KEY(user_id) REFERENCES users(id),
 CONSTRAINT fk_comments_images FOREIGN KEY(image_id) REFERENCES images(id)
 )ENGINE=InnoDb;
 
+INSERT INTO comments VALUES(NULL, 1, 4, 'Buena foto de familia', CURTIME(), CURTIME());
+INSERT INTO comments VALUES(NULL, 2, 2, 'Buena foto de playa !', CURTIME(), CURTIME());
+INSERT INTO comments VALUES(NULL, 2, 4, 'Que buena !!', CURTIME(), CURTIME());
+
 CREATE TABLE IF NOT EXISTS likes(
 id              int(255) auto_increment not null,
 user_id         int(255),
@@ -50,3 +64,9 @@ CONSTRAINT pk_likes PRIMARY KEY(id),
 CONSTRAINT fk_likes_users FOREIGN KEY(user_id) REFERENCES users(id),
 CONSTRAINT fk_likes_images FOREIGN KEY(image_id) REFERENCES images(id)
 )ENGINE=InnoDb;
+
+INSERT INTO likes VALUES(NULL, 1, 4, CURTIME(), CURTIME());
+INSERT INTO likes VALUES(NULL, 2, 4, CURTIME(), CURTIME());
+INSERT INTO likes VALUES(NULL, 3, 1, CURTIME(), CURTIME());
+INSERT INTO likes VALUES(NULL, 3, 2, CURTIME(), CURTIME());
+INSERT INTO likes VALUES(NULL, 2, 1, CURTIME(), CURTIME());
--- proyecto-laravel/routes/web.php
+++ proyecto-laravel/routes/web.php
@@ -11,6 +11,27 @@
 |
 */
 
+use App\Image;
+
 Route::get('/', function () {
+    $images = Image::all(); // saco todas las images de la base de datos
+    foreach($images as $image){
+        echo 'Image path: '.$image->image_path."<br/>";
+        echo 'description: '.$image->description."<br/>";
+        echo 'usuario: '.$image->user->name.' '.$image->user->surname."<br/>";
+
+        if(count($image->comments) >= 1 ){
+            echo "<strong>Comentarios: </strong>"."<br/>";
+            foreach($image->comments as  $comment){
+                echo $comment->user->nick.' : '.$comment->content."<br/>";
+            }
+        }
+
+        echo 'LIKES: '.count($image->likes);
+        //var_dump($image->user);
+        echo "<hr/>";
+        //var_dump($image); //Esto muestra todo vitaminadao (muchos arrays de todo)
+    }
+    die();
     return view('welcome');
 });
