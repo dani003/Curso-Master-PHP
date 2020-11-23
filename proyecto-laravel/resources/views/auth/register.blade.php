1ecf35a160e408068ab7af147baaa66b66a43c19
Daniela <daniela.ramirez003@gmail.com>
2020-11-23 05:02:14 -0300

    Date & clock

--- proyecto-laravel/resources/views/auth/register.blade.php
+++ proyecto-laravel/resources/views/auth/register.blade.php
@@ -26,6 +26,34 @@
                         </div>
 
                         <div class="form-group row">
+                            <label for="surname" class="col-md-4 col-form-label text-md-right">{{ __('Surame') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="surname" type="text" class="form-control{{ $errors->has('surname') ? ' is-invalid' : '' }}" name="surname" value="{{ old('surname') }}" required autofocus>
+
+                                @if ($errors->has('surname'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('surname') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="nick" class="col-md-4 col-form-label text-md-right">{{ __('Nick') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="nick" type="text" class="form-control{{ $errors->has('nick') ? ' is-invalid' : '' }}" name="nick" value="{{ old('nick') }}" required autofocus>
+
+                                @if ($errors->has('nick'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('nick') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
                             <div class="col-md-6">
