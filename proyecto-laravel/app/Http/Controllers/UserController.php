8cb5d05732ac5916cf4f97db047234bb475c8945
Daniela <daniela.ramirez003@gmail.com>
2021-01-06 10:30:21 -0300

    Actualizar datos

--- proyecto-laravel/app/Http/Controllers/UserController.php
+++ proyecto-laravel/app/Http/Controllers/UserController.php
@@ -11,17 +11,34 @@ class UserController extends Controller
     }
 
     public function update(Request $request){
+        //conseguir usuario identificado
+        $user = \Auth::user();
         $id = \Auth::user()->id;
 
+        //validacion del formulario
+        $validate = $this->validate($request, [
+            'name' => ['required', 'string', 'max:255'],
+            'surname' => ['required', 'string', 'max:255'],
+            'nick' => ['required', 'string', 'max:255', 'unique:users,nick,'.$id],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users,email,'.$id],
+        ]);
+
+        //Recoger los datos del formulario
         $name = $request->input('name');
         $surname = $request->input('surname');
         $nick = $request->input('nick');
         $email = $request->input('email');
 
-        var_dump($id);
-        var_dump($name);
-        var_dump($surname);
+        //Asignar nuevos valores al objeto del usuario 
+        $user->name = $name;
+        $user->surname = $surname;
+        $user->nick = $nick;
+        $user->email = $email;
 
-        die();
+        //Ejecutar consulta y cambios en la base de datos
+        $user->update();
+
+        return redirect()->route('config')
+                         ->with(['message'=>'Usuario actualizado correctamente']);
     }
 }
--- proyecto-laravel/resources/views/user/config.blade.php
+++ proyecto-laravel/resources/views/user/config.blade.php
@@ -4,6 +4,11 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
+            @if(session('message'))
+                <div class="alert alert-success" >
+                    {{ session('message')}}
+                </div>
+            @endif
             <div class="card">
                 <div class="card-header">Configuracion de mi cuenta</div>
 
