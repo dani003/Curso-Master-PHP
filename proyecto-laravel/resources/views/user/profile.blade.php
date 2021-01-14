cb78e67a57bf6c3193a3e053a97301e92f8797a4
Daniela <daniela.ramirez003@gmail.com>
2021-01-14 01:27:15 -0300

    perfil user

--- proyecto-laravel/resources/views/user/profile.blade.php
+++ proyecto-laravel/resources/views/user/profile.blade.php
@@ -4,23 +4,31 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div clas='data-user'>
 
-                @if ($user->image)
-                <div class='container-avatar'>
-                    <img src="{{ route('user.avatar', ['filename' => $user->image]) }}">
+            <div class="profile-user">
+
+                @if($user->image)
+                <div class="container-avatar">
+                    <img src="{{ route('user.avatar',['filename'=>$user->image]) }}" class="avatar" />
                 </div>
                 @endif
 
-                <div class='user-info'>
+                <div class="user-info">
                     <h1>{{'@'.$user->nick}}</h1>
-                    <h2>{{$user->name.' '.$user->surname}}</h2>
-                    <span class='nickname date'> {{'Se unió: '.\FormatTime::LongTimeFilter($user->created_at)}} </span>
+                    <h2>{{$user->name .' '. $user->surname}}</h2>
+                    <p>{{'Se unió: '.\FormatTime::LongTimeFilter($user->created_at)}}</p>
                 </div>
+
+                <div class="clearfix"></div>
+                <hr>
             </div>
-            @foreach ($user->images as $image)
-            @include('includes.image', ['image' =>$image])
+
+            <div class="clearfix"></div>
+
+            @foreach($user->images as $image)
+            @include('includes.image',['image'=>$image])
             @endforeach
+
 
         </div>
 
