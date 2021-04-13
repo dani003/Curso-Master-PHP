c05a70e35ea46dee29654cbc71530ac7d67ab3b4
Daniela <daniela.ramirez003@gmail.com>
2021-04-12 20:55:07 -0400

    Database conection and Entities

---
+++ proyecto-symfony/database.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE IF NOT EXISTS symfony_master;
+USE symfonmy_master;
+
+CREATE TABLE IF NOT EXISTS users{
+    id          int(255) auto_increment not null,
+    role        varchar(50),
+    name        varchar(100),
+    surname     varchar(50),
+    email       varchar(50),
+    password    varchar(50),
+    created_at  datetime,
+    CONSTRAINT pk_users PRIMARY KEY(id)
+}ENGINE=InnoDb;
+
+CREATE TABLE IF NOT EXISTS tasks{
+    id          int(255) auto_increment not null,
+    user_id     int(255) not null,
+    title       varchar(255),
+    content     text,
+    priority    varchar(20),
+    hours       int(100),
+    created_at  datetime,
+    CONSTRAINT pk_tasks PRIMARY KEY(id),
+    CONSTRAINT fk_task_user FOREIGN KEY(user_id) REFERENCES users(id)
+}ENGINE=InnoDb;
